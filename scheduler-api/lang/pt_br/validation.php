aa426d62072092e23e8e6be29713ef56d7952a24
Guilherme Pessoa <guilherme.gpessoa@hotmail.com>
2026-06-30 12:40:32 -0300

    fix(backend): localize framework validation messages to pt_BR

---
+++ scheduler-api/lang/pt_br/validation.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'accepted' => 'O campo :attribute deve ser aceito.',
+    'email' => 'O campo :attribute deve ser um endereço de e-mail válido.',
+    'unique' => 'O :attribute já está sendo utilizado.',
+    'min' => [
+        'string' => 'O campo :attribute deve ter pelo menos :min caracteres.',
+    ],
+    'required' => 'O campo :attribute é obrigatório.',
+    'confirmed' => 'A confirmação do campo :attribute não confere.',
+    
+    'attributes' => [
+        'email' => 'e-mail',
+        'password' => 'senha',
+        'name' => 'nome',
+        'role' => 'perfil',
+    ],
+];
